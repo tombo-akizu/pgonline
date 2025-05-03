5d0aed7b907a8c5a2eadf25928e4822bb2135043
tombo-akizu <akasaka-kotaro@keio.jp>
2025-05-04 01:56:20 +0900

    remove: キューブ回転の角速度を送信するコードの削除。

--- server/src/main.rs
+++ server/src/main.rs
@@ -39,8 +39,6 @@ async fn main() -> Result<(), Error> {
             let players = [p1, p2];
             tokio::spawn(game_2p(players));
         }
-
-        // tokio::spawn(accept_connection(stream));
     }
 
     Ok(())
@@ -163,59 +161,6 @@ async fn send_output(
             Err(tokio_tungstenite::tungstenite::Error::Protocol(tokio_tungstenite::tungstenite::error::ProtocolError::SendAfterClosing)) => {
                 break;
             }
-            _ => {}
-        }
-    }
-}
-
-
-async fn accept_connection(stream: TcpStream) {
-    let addr = stream.peer_addr().expect("connected streams should have a peer address");
-    info!("Peer address: {}", addr);
-
-    let ws_stream = tokio_tungstenite::accept_async(stream)
-        .await
-        .expect("Error during the websocket handshake occurred");
-
-
-    info!("New WebSocket connection: {}", addr);
-
-    let (mut write, mut read) = ws_stream.split();
-    // We should not forward messages other than text or binary.
-    // read.try_filter(|msg| future::ready(msg.is_text() || msg.is_binary()))
-    //     .forward(write)
-    //     .await
-    //     .expect("Failed to forward messages")
-
-    while let Some(msg_result) = read.next().await {
-        match msg_result {
-            Ok(Message::Binary(data)) => {
-                info!("Received binary: {:?}", data);
-
-                let response = match data.get(0) {
-                    Some(0x00) => vec![0x00],
-                    Some(0x01) => vec![0x01],
-                    Some(0x02) => vec![0x02],
-                    Some(0x03) => vec![0x00],
-                    _ => {
-                        info!("unknown byte...");
-                        vec![0x00]
-                    }
-                };
-
-                write.send(Message::Binary(Bytes::from(response))).await.unwrap();
-            }
-
-            Ok(Message::Text(text)) => {
-                info!("Received text: {}", text);
-                write.send(Message::Text("Binary only!".into())).await.unwrap();
-            }
-
-            Ok(Message::Close(_)) => {
-                info!("Connection closed");
-                break;
-            }
-
             _ => {}
         }
     }
